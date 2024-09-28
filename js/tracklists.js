9006e2a38a3653af7ff0614da0c2103209de5845
max <leonardoretardo420@gmail.com>
2024-09-28 20:46:30 +0300

    music section finished

--- js/tracklists.js
+++ js/tracklists.js
@@ -1,47 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
     const PATH = 'https://api.github.com/repos/maxergun/maxergun.github.io/contents/assets/audio/'
-    const PLAY_BTN_IMG = 'https://api.github.com/repos/maxergun/maxergun.github.io/contents/assets/img/pic.png'
-    const TOKEN = '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'
-    const containerElem = document.getElementById("tracklist-container")
+
+    const containerElem = document.getElementById('tracklistContainer')
+
+    const MUSIC_PLAY_IMG_ID = 'icon-music'
+    const _musicPlayImgElem = document.getElementById(MUSIC_PLAY_IMG_ID)
+    const _musicPlayImgActiveElem = document.getElementById('icon-music-active')
+
     let currentTracklistElem
+    let currentTrackPlayImgElem
 
     function CreateTrackElement(data) {
         const fullName = data.name
         const trackName = fullName.substring(0, fullName.length - 4)
+
         const trackElem = document.createElement("li")
         trackElem.className = 'mp3'
 
         const nameElem = document.createElement("p")
         nameElem.innerHTML = trackName
 
-        const playbtnElem = document.createElement("img")
-        playbtnElem.src = PLAY_BTN_IMG
+        const playImgElem = document.createElement("img")
+        _musicPlayImgElem.id = MUSIC_PLAY_IMG_ID
 
-        trackElem.appendChild(playbtnElem)
+        playImgElem.src = _musicPlayImgElem.src
+        
+        trackElem.appendChild(playImgElem)
         trackElem.appendChild(nameElem)
         currentTracklistElem.appendChild(trackElem)
+        
+        trackElem.addEventListener('click', function() {
+            if (currentTrackPlayImgElem) {
+                currentTrackPlayImgElem.src = _musicPlayImgElem.src
+            }
+            playImgElem.src = _musicPlayImgActiveElem.src
+            currentTrackPlayImgElem = playImgElem
+
+            const setMusicEvent = new CustomEvent("setMusic", {detail: {
+                url: data.download_url,
+                title: trackName
+            }})
+            setMusicEvent.isTrusted = true
+            setMusicEvent.target
+            document.dispatchEvent(setMusicEvent) 
+        })
     }
 
-    function InitAudioFolder(url) {
-        const headers = 
+    function InitAudioFolder(url, title, bgElemId) {
         fetch(url, {headers: {
             Authorization: 'token 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' 
         }})
         .then(response => response.json()) 
         .then(data => {
             const tracklistElem = document.createElement('ul')
-                tracklistElem.className = 'tracklist'
-                containerElem.appendChild(tracklistElem)
-                currentTracklistElem = tracklistElem
-                data.forEach(item => {
-                    if (item.type == 'file' && item.name.endsWith(".mp3")) {
-                        CreateTrackElement(item)
-                    }
-                })
+            tracklistElem.className = 'tracklist media-scroller'
+
+            const div = document.createElement("div")
+            const titleElem = document.createElement("header")
+            div.appendChild(titleElem)
+            div.appendChild(tracklistElem)
+            titleElem.innerHTML = '<h text-align: center; style="color: black" width: 100%>' + title + '</h>'
+            div.style = "display: grid;"
+            
+            containerElem.appendChild(div)
+            currentTracklistElem = tracklistElem
+            data.forEach(item => {
+                if (item.type == 'file' && item.name.endsWith(".mp3")) {
+                    CreateTrackElement(item)
+                }
+            })
             }).catch(error => console.error('Error fetching files:', error))
     }
 
-    InitAudioFolder(PATH + 'crawlyard')
+    InitAudioFolder(PATH + 'crawlyard', "Action (Electronic)") 
+    InitAudioFolder(PATH + 'crawlyard', "Rock")
 
     // const music = fetch(musicPath)
 
